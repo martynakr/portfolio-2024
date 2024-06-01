81346630b925094f537dc7c8cfba4190bba08538
Martyna Król <martyna.krool@gmail.com>
2024-06-01 11:15:48 +1000

    intro page draft

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1>Martyna Krol</h1>
+            <h3>Software Developer | Educator</h3>
+            <p>
+                Check out out my work on
+                <a href="https://github.com/martynakr">Github</a>
+            </p>
+        </>
+    );
+}
+
+export default App;
